d632783a0025e041800b37b2c2f412a4d0d26c3b
Kateryna Myskova <kateryna.myskova@stud.unilu.ch>
2021-11-21 15:26:10 +0100

    Corrected latest errors and edited some files

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,12 @@ FROM python:3.8
 
 WORKDIR /app
 
-COPY code/*.py modules/
+COPY code *.py modules/
 COPY requirements.txt .
+COPY Task4.py .
 
 RUN pip install -r requirements.txt
+# RUN docker run --name=postgre_vol -d -v ~/postgre_vol:/var/lib/postgresql/data -p 5050:80 postgres
 
 CMD python modules/main.py
+CMD Task4.py
---
+++ Task4.py
@@ -0,0 +1,100 @@
+# laying out the basis from previous tasks and/or Milestones
+
+import psycopg2
+import numpy as np
+from tensorflow import keras
+import pickle
+from PIL import Image
+
+
+host = "localhost"
+database = "milestone_3"
+port = "5432"
+user = "admin"
+password = "1234"
+
+
+con = psycopg2.connect(
+    host="localhost",
+    port="5432",
+    database="milestone_3",
+    user="admin",
+    password="1234")
+
+cur = con.cursor()
+
+# creating the input data table
+cur.execute('''
+    CREATE TABLE input_data (
+    ID SERIAL PRIMARY KEY,
+    input_label TEXT);
+    ''')
+
+# creating the predictions table
+cur.execute('''
+    CREATE TABLE predictions (
+    ID SERIAL PRIMARY KEY,
+    prediction TEXT);
+    ''')
+
+# import already existing module
+from data_load import load_data
+
+
+# Load and store the sample image from data
+
+# [1]
+# load data
+x_train, y_train, x_test, y_test = load_data()
+
+# take first picture of training data as sample
+sample = x_train[0]
+
+# Visualize picture
+Image.fromarray(sample).show()
+
+# Transform picture to byte stream
+sample_bytes = pickle.dumps(sample)
+
+cur = con.cursor()
+cur.execute(
+    """
+    INSERT INTO input_data (Picture)
+    VALUES (%s)
+    """,
+    (sample_bytes, ))
+
+# [2]
+# take first picture of training data as sample
+sample = x_train[0]
+
+# Visualize picture
+Image.fromarray(sample).show()
+
+# Transform picture to byte stream
+sample_bytes = pickle.dumps(sample)
+
+cur = con.cursor()
+cur.execute(
+    """
+    INSERT INTO predictions (Picture)
+    VALUES (%s)
+    """,
+    (sample_bytes, ))
+
+# executing query
+cur.execute("select * from input_data;")
+print("The sample input tested:")
+print(cur.fetchall())
+
+
+cur.execute("select * from predictions;")
+print("The produced prediction from the tested sample:")
+print(cur.fetchall())
+
+
+# commit data to db
+con.commit()
+
+
+con.close()
